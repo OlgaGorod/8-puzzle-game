fd6619861210dc5ca9c428ac594eb2d9bc358e05
Olga <Olga@MSI>
2021-07-28 14:00:47 +0300

    Add project files.

---
+++ пятнашки с числами/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace пятнашки_с_числами
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.NewGame = new System.Windows.Forms.Button();
+            this.GoOut = new System.Windows.Forms.Button();
+            this.instruction = new System.Windows.Forms.Label();
+            this.CountOfSteps = new System.Windows.Forms.Label();
+            this.labelSteps = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LimeGreen;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(12, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LimeGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(68, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LimeGreen;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(124, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 1;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LimeGreen;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(12, 70);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 1;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LimeGreen;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(68, 71);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 1;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.LimeGreen;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(124, 71);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 1;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LimeGreen;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.Location = new System.Drawing.Point(12, 126);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 1;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.LimeGreen;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.Location = new System.Drawing.Point(68, 127);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 1;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.LimeGreen;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.Location = new System.Drawing.Point(124, 127);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 1;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // NewGame
+            // 
+            this.NewGame.BackColor = System.Drawing.Color.Gold;
+            this.NewGame.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.NewGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.NewGame.Location = new System.Drawing.Point(318, 15);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(115, 30);
+            this.NewGame.TabIndex = 0;
+            this.NewGame.Text = "НОВАЯ ИГРА";
+            this.NewGame.UseVisualStyleBackColor = false;
+            this.NewGame.Click += new System.EventHandler(this.NewGame_Click);
+            // 
+            // GoOut
+            // 
+            this.GoOut.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.GoOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GoOut.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.GoOut.Location = new System.Drawing.Point(318, 51);
+            this.GoOut.Name = "GoOut";
+            this.GoOut.Size = new System.Drawing.Size(115, 31);
+            this.GoOut.TabIndex = 10;
+            this.GoOut.Text = "ПОКИНУТЬ ИГРУ";
+            this.GoOut.UseVisualStyleBackColor = false;
+            this.GoOut.Click += new System.EventHandler(this.GoOut_Click);
+            // 
+            // instruction
+            // 
+            this.instruction.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.instruction.ForeColor = System.Drawing.SystemColors.Info;
+            this.instruction.Location = new System.Drawing.Point(233, 90);
+            this.instruction.Name = "instruction";
+            this.instruction.Size = new System.Drawing.Size(200, 58);
+            this.instruction.TabIndex = 3;
+            this.instruction.Text = "Вам необходимо расположить цифры в порядке возрастания слева направо сверху вниз";
+            this.instruction.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // CountOfSteps
+            // 
+            this.CountOfSteps.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.CountOfSteps.Location = new System.Drawing.Point(265, 172);
+            this.CountOfSteps.Name = "CountOfSteps";
+            this.CountOfSteps.Size = new System.Drawing.Size(158, 13);
+            this.CountOfSteps.TabIndex = 4;
+            this.CountOfSteps.Text = "Количество сделанных ходов";
+            this.CountOfSteps.UseMnemonic = false;
+            // 
+            // labelSteps
+            // 
+            this.labelSteps.AutoSize = true;
+            this.labelSteps.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.labelSteps.Location = new System.Drawing.Point(420, 172);
+            this.labelSteps.Name = "labelSteps";
+            this.labelSteps.Size = new System.Drawing.Size(13, 13);
+            this.labelSteps.TabIndex = 11;
+            this.labelSteps.Text = "0";
+            this.labelSteps.Click += new System.EventHandler(this.labelSteps_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(445, 190);
+            this.Controls.Add(this.labelSteps);
+            this.Controls.Add(this.CountOfSteps);
+            this.Controls.Add(this.instruction);
+            this.Controls.Add(this.GoOut);
+            this.Controls.Add(this.NewGame);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(5000, 1500);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Пятнашки с числами";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button NewGame;
+        private System.Windows.Forms.Button GoOut;
+        private System.Windows.Forms.Label instruction;
+        private System.Windows.Forms.Label CountOfSteps;
+        private System.Windows.Forms.Label labelSteps;
+    }
+}
+
---
+++ пятнашки с числами/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace пятнашки_с_числами
+{
+    public partial class FormMain : Form
+        {
+            Boolean ifStarted = new Boolean();
+            int[] fArray = new int[9];
+            Button[] myButtons; 
+            MyRand rand = new MyRand();
+            C_MovesOfButtons move = new C_MovesOfButtons();
+
+
+        public FormMain()
+        {
+            InitializeComponent();
+            CountOfSteps.Visible = false;
+            labelSteps.Visible = false;
+            ifStarted = false;
+        }
+        public void makeInviseble(Button[] myBut)//делает 0 кнопку невидимой
+        {
+            for (int i = 0; i < 9; i++)
+                if (int.Parse(myBut[i].Text)==0)
+                    myBut[i].Visible = false;
+                else
+                    myBut[i].Visible = true;
+        }
+            public void re()///обновление цифр на кнопках
+          {
+            for (int i = 0; i < 9; i++)
+                myButtons[i].Text = fArray[i].ToString();
+            labelSteps.Text= move.StepsCounter().ToString();//обновление счётчика ходов
+            Win win = new Win();
+            if (win.ifWin(fArray))//проверка на выигрышную кобинацию 
+            {
+                makeInviseble(myButtons);
+                DialogResult dr = MessageBox.Show("Ещё разок?", "Победа!", MessageBoxButtons.YesNo);
+                if (dr == DialogResult.Yes)//При желании игрока сыграть ещё раз
+                    NewGame.PerformClick();//начинается новая игра
+                else
+                    this.Close();
+            }
+            makeInviseble(myButtons);
+        }
+        private void NewGame_Click(object sender, EventArgs e)//кнопка начала игры
+        {
+            ifStarted = true;
+            labelSteps.Visible = true;
+            CountOfSteps.Visible = true;
+            move.restart();//метод, сбрасывающий счётчик ходов
+            fArray = rand.toRand();//формирование массива случайных чисел
+
+            //массив кнопок:
+            myButtons = new Button[9] { button1, button2, button3, button4, button5, button6, button7, button8, button9 };
+            for (int i = 0; i < 9; i++)
+                myButtons[i].Text = fArray[i].ToString();//нанесение на кнопки случайного массива цифр
+            makeInviseble(myButtons);//
+            labelSteps.Text = "0";
+        }
+        private void GoOut_Click(object sender, EventArgs e)//кнопка выхода
+        {
+            DialogResult dialRes = MessageBox.Show("Вы уверены, что хотите выйти?", "Выйти?!", MessageBoxButtons.YesNo);
+            if (dialRes == DialogResult.Yes)//проверка
+                this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ifStarted)//если новая игра начата
+            {
+                fArray = move.MoveNumb(fArray, 0);//передвижение чисел в массиве
+                this.re();//обновление цифр на кнопках
+            }
+        }
+            private void button2_Click(object sender, EventArgs e)
+        {
+           
+            if (ifStarted)
+            {
+                fArray = move.MoveNumb(fArray, 1);
+                this.re();
+            }
+        }
+            private void button3_Click(object sender, EventArgs e)
+        {
+            
+            if (ifStarted)
+            {
+                fArray = move.MoveNumb(fArray, 2);
+                this.re();
+            }
+        }
+            private void button4_Click(object sender, EventArgs e)
+        {
+            
+            if (ifStarted)
+            {
+                fArray = move.MoveNumb(fArray, 3);
+                this.re();
+            }
+        }
+            private void button5_Click(object sender, EventArgs e)
+            {
+            
+            if (ifStarted)
+                {
+                    fArray = move.MoveNumb(fArray, 4);
+                    this.re();
+            }
+            }
+            private void button6_Click(object sender, EventArgs e)
+        {
+            
+            if (ifStarted)
+            {
+                fArray = move.MoveNumb(fArray, 5);
+                this.re();
+            }
+        }
+            private void button7_Click(object sender, EventArgs e)
+            {
+            
+            if (ifStarted)
+            {
+                fArray = move.MoveNumb(fArray, 6);
+                this.re();
+            }
+            }
+            private void button8_Click(object sender, EventArgs e)
+            {
+            
+            if (ifStarted)
+            {
+                fArray = move.MoveNumb(fArray, 7);
+                this.re();
+            }
+            }
+            private void button9_Click(object sender, EventArgs e)
+            {
+            
+            if (ifStarted)
+                {
+                    fArray = move.MoveNumb(fArray, 8);
+                     this.re();
+            }
+            }
+
+        private void labelSteps_Click(object sender, EventArgs e)//будем считать пасхалочкой
+        {
+            MessageBox.Show("Учитываются все передвижения цифр. Даже туда-обратно","Внимание!");
+        }
+    }
+
+    public class C_MovesOfButtons
+    {
+        static int allSteps = 0;//счётчик ходов
+        static int[] arrayOfNumbers = new int[9];
+        static int k = 0;//переменная для запоминания, с цифрой какой кнопки нужно поменять цифру нажатой кнопки
+        static Boolean isMoved;
+
+        public int StepsCounter()//счётчик передвижений
+        {
+            if (isMoved == true)
+                allSteps++;
+            return allSteps;
+        }
+        public void restart()//сбрасывание счётчика
+        {
+            if (allSteps != 0)
+                allSteps = 0;
+        }
+        public int[] MoveNumb(int[] arrayOfNumb, int butNumb)//перемещение
+        {
+            arrayOfNumbers = arrayOfNumb;
+            isMoved = false;
+            //проверка условия наличия нулевой клетки с одной из сторон (справа, слева, внизу или сверху) для перемещения
+            //при выполнении условия цифры меняются местами в соответствующем массиве
+            if (butNumb != 2 && butNumb != 5 && butNumb != 8)//для правой кнопки
+                if (arrayOfNumbers[butNumb + 1] == 0)
+                {
+                    k = arrayOfNumbers[butNumb];
+                    arrayOfNumbers[butNumb] = arrayOfNumbers[butNumb + 1];
+                    arrayOfNumbers[butNumb + 1] = k;
+                    isMoved = true;
+                    k = 0;
+                }
+            if (butNumb < 6)//для нижней кнопки
+                if (arrayOfNumbers[butNumb + 3] == 0)
+                {
+                    k = arrayOfNumbers[butNumb];
+                    arrayOfNumbers[butNumb] = arrayOfNumbers[butNumb + 3];
+                    arrayOfNumbers[butNumb + 3] = k;
+                    isMoved = true;
+                    k = 0;
+                }
+            if (butNumb != 0 && butNumb != 3 && butNumb != 6)//для левой кнопки
+                if (arrayOfNumbers[butNumb - 1] == 0)
+                {
+                    k = arrayOfNumbers[butNumb];
+                    arrayOfNumbers[butNumb] = arrayOfNumbers[butNumb - 1];
+                    arrayOfNumbers[butNumb - 1] = k;
+                    isMoved = true;
+                    k = 0;
+                }
+            if (butNumb > 2)//для верхней кнопки
+                if (arrayOfNumbers[butNumb - 3] == 0)
+                {
+                    k = arrayOfNumbers[butNumb];
+                    arrayOfNumbers[butNumb] = arrayOfNumbers[butNumb - 3];
+                    arrayOfNumbers[butNumb - 3] = k;
+                    isMoved = true;
+                    k = 0;
+                }
+            return arrayOfNumbers; //возвращение массива с обновленной расстановкой цифр
+        }
+    }
+
+    public class MyRand
+        {
+            public int[] toRand()//рандомайзер
+            {
+                int[] fArray = new int[9]; 
+                Random _random = new Random(); 
+                int elementsofArray = 0; // переменная, считающая уже заданные элементы массива
+                int rand = _random.Next(1, 10); // генерация числа с записью в переменную
+                do
+                {
+                    int addElement = 0;
+                    // проверка на совпадение вновь сгенерированного числа с любым из массива:
+                    for (int i = 0; i < fArray.Length; i++)
+                    {
+                        if (fArray[i] != rand)
+                            addElement++;
+                        else
+                            addElement--;
+                    }
+                    // если совпадений не найдено, то последнее сгенерированное число запсывается в следующую переменную
+                    if (addElement == 9)
+                    {
+                        fArray[elementsofArray] = rand;
+                        elementsofArray++;
+                    }
+                    // генерирование нового случайного числа, с которым потом будут сравниваться все уже существующие в массиве числа
+                    rand = _random.Next(1, 10);
+                }
+                while (elementsofArray != 9); // генерация выполняется, пока в массиве не будет 8 уникальных чисел
+                for (int i = 0; i < 9; i++)
+                    if (fArray[i] == 9) fArray[i] = 0;//обозначение "пустой" клетки
+                return fArray;
+            }
+        }
+
+    public class Win
+        {
+            Boolean win = false;
+            int k = 0;
+            public Boolean ifWin(int[] arBut)
+            {
+                int[] arrayWin = new int[9] { 1, 2, 3, 4, 5, 6, 7, 8, 0 };//выигрышная расстановка цифр в массиве
+                for (int i = 0; i < 9; i++)
+                    if (arBut[i] == arrayWin[i])
+                        k++;
+                if (k == 9) win = true;
+                return win;
+            }
+        }
+}
